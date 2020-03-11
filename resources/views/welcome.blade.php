ecf747bad83bcf6fd0f278d72475fa4e05e675a3
uranoni <uranoni777@gmail.com>
2020-03-11 09:05:28 +0800

    ++ api/route timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    @lang('messages.welcome')
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('welcome', function (Request $request) {
+    $locale = Str::of($request->server('HTTP_ACCEPT_LANGUAGE'))->explode(',')->first();
+    App::setLocale($locale);
+    return view('welcome');
+
 });
